8ddd8dd44648636e9be45dbd9b9e42949529b47d
SomeRandomBlackGuy <35743459+SomeRandomBlackGuy@users.noreply.github.com>
2018-02-17 12:24:55 -0700

    New Game On

---
+++ GameOn3x.cpp
@@ -0,0 +1,19 @@
+// Game Over
+// A first C++ program
+#include "stdafx.h"
+#include <iostream> // preprocessor for the library stream (Helps display text)
+using namespace std;
+
+
+int main() // Header function returns an integer
+{  //Beginning of function 
+	cout << "Game On!" << endl;
+	//std : name space in the stream library
+	//cout : an object that sends output to the library stream
+	// ::scope resolution operator (used to prefix a name space)
+	// << : output operators sends string to cout
+	// "" : String literal
+	//end1: predefined in the library ()
+	system("pause");
+	return 0;
+} // Marks the end of the function
